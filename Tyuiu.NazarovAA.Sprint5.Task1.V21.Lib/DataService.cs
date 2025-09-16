53255c382767b005e9cedf3c211015a1220cd4cf
NazarovAA <Tyuiu.NazarovAA@outlook.com>
2025-09-17 00:52:34 +0500

    Task1

---
+++ Tyuiu.NazarovAA.Sprint5.Task1.V21.Lib/DataService.cs
@@ -0,0 +1,36 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.NazarovAA.Sprint5.Task1.V21.Lib
+{
+    public class DataService : ISprint5Task1V21
+    {
+        public string SaveToFileTextData(int startValue, int stopValue)
+        {
+            double[] res = new double[stopValue - startValue + 1];
+
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask1.txt");
+            FileInfo fileInfo = new FileInfo(path);
+
+            if (fileInfo.Exists)
+                File.Delete(path);
+
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                if (Math.Cos(x) - 2 * x == 0)
+                    res[x - startValue] = 0;
+                else
+                    res[x - startValue] = Math.Round((2 * x - 3) / (Math.Cos(x) - 2 * x) + 5 * x - Math.Sin(x), 2);
+            }
+
+            for (int i = 0; i < res.Length; i++)
+            {
+                if (i != res.Length - 1)
+                    File.AppendAllText(path, res[i] + Environment.NewLine);
+                else
+                    File.AppendAllText(path, res[i].ToString());
+            }
+
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.NazarovAA.Sprint5.Task1.V21/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Tyuiu.NazarovAA.Sprint5.Task1.V21.Lib;
+
+namespace Tyuiu.NazarovAA.Sprint5.Task1.V21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Назаров А. А. | РППб 25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись набора данных в текстовый файл                 *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #21                                                             *");
+            Console.WriteLine("* Выполнил: Назаров Артём Александрович | СППб-25-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Сохранить результат вычисления в таблицу и вывести на экран [-5, 5]     *");
+            Console.WriteLine("*               2x - 3                                                    *");
+            Console.WriteLine("*    F(x) = -------------- + 5x + sin(x)                                  *");
+            Console.WriteLine("*            cos(x) - 2x                                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x1 = -5;
+            int x2 = 5;
+            Console.WriteLine("x1 = " + x1);
+            Console.WriteLine("x2 = " + x2);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.SaveToFileTextData(-5, 5));
+            Console.WriteLine("Файл успешно создан!");
+
+            string res = File.ReadAllText(Path.Combine(Path.GetTempPath(), "OutPutFileTask1.txt"));
+
+            string[] mas = res.Split("\n");
+            Console.WriteLine("+---------+---------+");
+            Console.WriteLine("|    X    |   f(X)  |");
+            Console.WriteLine("+---------+---------+");
+            for (int i = x1; i <= x2; i++)
+            {
+                string value = mas[i - x1].Trim();
+                Console.WriteLine("|{0, 5:d}    |{1, 7:f2}  |", i, value);
+            }
+            Console.WriteLine("+---------+---------+");
+
+            //Console.WriteLine(res);
+        }
+    }
+}
