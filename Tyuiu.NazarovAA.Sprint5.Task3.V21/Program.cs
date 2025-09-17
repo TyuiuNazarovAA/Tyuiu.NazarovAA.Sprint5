36810bbe435e5abcbe15c8f6973134be73a76daa
NazarovAA <Tyuiu.NazarovAA@outlook.com>
2025-09-17 10:27:20 +0500

    Task3

---
+++ Tyuiu.NazarovAA.Sprint5.Task3.V21/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.NazarovAA.Sprint5.Task3.V21.Lib;
+
+namespace Tyuiu.NazarovAA.Sprint5.Task3.V21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Назаров А. А. | РППб 25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Потоковый метод записи данных в бинарный файл                     *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #21                                                             *");
+            Console.WriteLine("* Выполнил: Назаров Артём Александрович | СППб-25-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано выражение вычислить его значение при x = 3, результат сохранить в  *");
+            Console.WriteLine("* бинарный файл OutPutFileTask3.bin и вывести на консоль. Округлить до    *");
+            Console.WriteLine("* трёх знаков после запятой.                                              *");
+            Console.WriteLine("*              2                                                          *");
+            Console.WriteLine("*             x  + 1                                                      *");
+            Console.WriteLine("*    y(x) = --------------                                                *");
+            Console.WriteLine("*             ---------                                                   *");
+            Console.WriteLine("*             |  2                                                        *");
+            Console.WriteLine("*            V 4x  - 3                                                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x = 3;
+            Console.WriteLine("x = " + x);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.SaveToFileTextData(x));
+            Console.WriteLine("Файл успешно создан!");
+        }
+    }
+}
